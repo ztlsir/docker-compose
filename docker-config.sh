a32e0c64ef9c1f170cc2adc7d3bb0a0143202a66
ztlsir <584821699@qq.com>
2020-05-17 12:52:32 +0800

    save cahnge

--- docker-config.sh
+++ docker-config.sh
@@ -1,3 +1,3 @@
 echo 'Begin create docker LAN'
-docker network create --subnet=172.18.0.0/16 -d bridge lan
+docker network create --subnet=172.18.0.1/16 -d bridge lan
 echo 'Docker LAN is created,and name is lan'
